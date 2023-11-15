c34f7a45a63ae3f1bfc80e4347a0e30eb059348c
Sophia Krugler <krugler@usc.edu>
2023-11-14 21:03:33 -0800

    Downloaded and adjusted example springboot project.

---
+++ springboot-backend/src/main/java/com/example/springbootbackend/SpringbootBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+}
