3128c5833cae21a5933ca184dbfbe251e15b9858
Yunrong Ma <yunrongm@usc.edu>
2023-11-19 13:40:48 -0800

    Profile.js modified

--- src/pages/profilepage/Profile.js
+++ src/pages/profilepage/Profile.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './style.css';
-import Header from "../Header/Header";
+import Header from "../Header";
 import {useNavigate} from"react-router-dom";
 
 const ProfilePage = () => {
     const navigate = useNavigate();
+
+    const tickets = [
+        { name: 'Name#1', location: 'Location#1', time: 'Time#1'},
+        { name: 'Name#2', location: 'Location#2', time: 'Time#2'},
+        { name: 'Name#3', location: 'Location#3', time: 'Time#3'}
+    ];
 
     return (
         <div>
@@ -12,20 +18,23 @@ const ProfilePage = () => {
             <br/>
             <br/>
 
-        <div className="container">
-            <div className="welcome">
-                <p>Welcome!</p>
-                <p>USC Email: <input type="email" value="USCEmail" disabled /></p>
-                <p>Password: <input type="password" value="******" disabled /></p>
-            </div>
+            <div className="container">
+                <div className="welcome">
+                    <p>Welcome!</p>
+                    <p>USC Email: <input type="email" value="USCEmail" disabled /></p>
+                    <p>Password: <input type="password" value="******" disabled /></p>
+                </div>
 
-            <div className="ticket">
-                <p>Your Tickets:</p>
-                <div>Name#1 Location#1 Time#1</div>
-                <div>Name#2 Location#2 Time#2</div>
-                <div>Name#3 Location#3 Time#3</div>
+                <div className="ticket">
+                    <p>Your Tickets:</p>
+                    {tickets.map(
+                        (ticket, index) => (
+                            <div key={index}>{`${ticket.name} ${ticket.location} ${ticket.time}`}
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
-        </div>
         </div>
     )
 }
