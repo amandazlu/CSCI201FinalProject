9d4e3b5ea1412b84dc741704508043dc572c51b4
Yunting Ma <yuntingm@usc.edu>
2023-11-27 20:52:45 -0800

    eventpage updated

--- react-frontend/src/pages/EventPage/EventPage.js
+++ react-frontend/src/pages/EventPage/EventPage.js
@@ -11,41 +11,48 @@ function Event() {
     return (
         <div>
 			<Header/>
-			<br/>
-			<br/>
 			<div className="event">
 				<div className="eventhead">
 					<div className="eventhead-text">
-						<h2>{eventName}</h2>
-						<p>Los Angeles, CA</p>
-						<h2 className="date">xx/xx/xxxx</h2>
+						<p style = {{fontSize : "3vw", textAlign : "left" , padding : "3%"}}>{eventName}</p>
+						<p style = {{fontSize : "2vw", textAlign : "left" , padding : "3%"}}>Los Angeles, CA</p>
+						<p className="date" style = {{fontSize : "2vw", textAlign : "right" , padding : "3%"}}>xx/xx/xxxx</p>
 					</div>
 				</div>
 				<div className="eventdescrip">
-					<h4>Venue Address:</h4>
-					<p>{eventVenue}</p>
-					<h4>Description:</h4>
-					<p>{eventDescrip}</p>
+					<p style = {{fontSize : "2vw", textAlign : "left" , padding : "3%"}}>Venue Address:</p>
+					<p style = {{fontSize : "1vw", textAlign : "left" , padding : "3%"}}>{eventVenue}</p>
+					<p style = {{fontSize : "2vw", textAlign : "left" , padding : "3%"}}>Description:</p>
+					<p style = {{fontSize : "1vw", textAlign : "left" , padding : "3%"}}>{eventDescrip}</p>
 				</div>
 			</div>
-			<div className="tickets">
-				<h3>Tickets</h3>
-				<div className="ticket">
-					<h3>General Admission</h3>
-					<p>140/200 available</p>
-					<button>Reserve</button>
-				</div>
-				<div className="ticket">
-					<h3>Floor Tickets</h3>
-					<p>50/50 available</p>
-					<button>Reserve</button>
-				</div>
-				<div className="ticket">
-					<h3>VIP Admission</h3>
-					<p>50/50 available</p>
-					<button>Reserve</button>
-				</div>
+			<div className="type">
+				<p style = {{fontWeight: "bold", fontSize: "3vw", textAlign: "left", padding: "3%"}}>Tickets</p>
+			    <div className="grid">
+			        <div className="ticket">
+			            <p className="event-header">General Admission</p>
+			            <p className="event-location">140/200 available</p>
+			            <div className="box">
+			                <button className="reserve-button">Reserve</button>
+			            </div>
+			        </div>
+			        <div className="ticket">
+			            <p className="event-header">Floor Tickets</p>
+			            <p className="event-location">50/50 available</p>
+			            <div className="box">
+			                <button className="reserve-button">Reserve</button>
+			            </div>
+			        </div>
+			        <div className="ticket">
+			            <p className="event-header">VIP Admission</p>
+			            <p className="event-location">50/50 available</p>
+			            <div className="box">
+			                <button className="reserve-button">Reserve</button>
+			            </div>
+			        </div>
+			    </div>
 			</div>
+
 		</div>
     );
 }
