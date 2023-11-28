7044eb186e90c103a25e982939cc31b2dbf9d310
Yunting Ma <97552275+Cathmyt@users.noreply.github.com>
2023-11-28 00:08:55 -0800

    Update EventPage.js! YAY

--- react-frontend/src/pages/EventPage/EventPage.js
+++ react-frontend/src/pages/EventPage/EventPage.js
@@ -1,15 +1,46 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
 import "./EventCss.css"
 import Header from "../Header/Header";
 import {useNavigate, useLocation} from"react-router-dom";
+import axios from 'axios';
+
 
 function Event(props) {
 	const {state} = useLocation();
 	const {id} = state;
+	const [eventData, setEventData] = useState(null);
+
+    useEffect(() => {
+	  if (id) {
+	    const apiRoute = "http://localhost:8080/api/v1/homepage";
+	    axios.get(apiRoute)
+	      .then(response => {
+	        // Assuming response.data is an array of objects
+	        const event = response.data.find(event => event.id === id);
+	        setEventData(event);
+	        console.log(event);
+	      })
+	      .catch(error => {
+	        console.log('Error fetching data:', error);
+	      });
+	  }
+	}, [id]);
+
+
 	const navigate = useNavigate;
-	const eventName = "Event 1"
-	const eventVenue = "Lorem ipsum address";
-	const eventDescrip = "Lorem ipsum dolor sit amet, consectetur adipiscing elit...";
+	const eventName = eventData ? eventData.eventName : "Loading...";
+    const eventVenue = eventData ? eventData.eventLocation : "Loading...";
+    const eventDescrip = eventData ? eventData.eventDescription : "Loading...";
+    const eventDate = eventData ? 
+                  `${eventData.eventDate.slice(0, 2)}/${eventData.eventDate.slice(2, 4)}/${eventData.eventDate.slice(4)}` : 
+                  "Loading...";
+    const eventTime = eventData ? eventData.eventTime : "Loading...";
+    const floorTicketPrice = eventData ? eventData.floorTicketPrice : "Loading...";
+    const floorTicketsLeft = eventData ? eventData.floorTicketsLeft : "Loading...";
+    const genadTicketPrice = eventData ? eventData.genadTicketPrice : "Loading...";
+    const genadTicketsLeft = eventData ? eventData.genadTicketsLeft : "Loading...";
+    const vipTicketPrice = eventData ? eventData.vipTicketPrice : "Loading...";
+    const vipTicketsLeft = eventData ? eventData.vipTicketsLeft : "Loading...";
     return (
         <div>
 			<Header/>
@@ -17,8 +48,8 @@ function Event(props) {
 				<div className="eventhead">
 					<div className="eventhead-text">
 						<p style = {{fontSize : "3vw", textAlign : "left" , padding : "3%"}}>{eventName}</p>
-						<p style = {{fontSize : "2vw", textAlign : "left" , padding : "3%"}}>Los Angeles, CA</p>
-						<p className="date" style = {{fontSize : "2vw", textAlign : "right" , padding : "3%"}}>xx/xx/xxxx</p>
+						<p style = {{fontSize : "2vw", textAlign : "left" , padding : "3%"}}>{eventVenue}</p>
+						<p className="date" style = {{fontSize : "2vw", textAlign : "right" , padding : "3%"}}>{eventDate} {eventTime}</p>
 					</div>
 				</div>
 				<div className="eventdescrip">
@@ -32,22 +63,25 @@ function Event(props) {
 				<p style = {{fontWeight: "bold", fontSize: "3vw", textAlign: "left", padding: "3%"}}>Tickets</p>
 			    <div className="grid">
 			        <div className="ticket">
-			            <p className="event-header" style = {{fontWeight: "bold", fontSize: "1.5vw", textAlign: "left", padding: "3%", color:"black"}}>General Admission</p>
-			            <p className="event-location" style = {{fontWeight: "bold", fontSize: "1vw", textAlign: "left", padding: "3%", color:"black"}}>140/200 available</p>
+			            <p className="event-header" style = {{fontWeight: "bold", fontSize: "1.5vw", textAlign: "left", padding: "3%", color:"black"}}>Floor Tickets</p>
+			            <p className="event-price" style = {{fontWeight: "bold", fontSize: "1vw", textAlign: "left", padding: "3%", color:"black"}}>Price: ${floorTicketPrice}</p>
+			            <p className="event-location" style = {{fontWeight: "bold", fontSize: "1vw", textAlign: "left", padding: "3%", color:"black"}}>{floorTicketsLeft} available</p>
 			            <div className="box">
 			                <button className="reserve-button">Reserve</button>
 			            </div>
 			        </div>
 			        <div className="ticket">
-			            <p className="event-header" style = {{fontWeight: "bold", fontSize: "1.5vw", textAlign: "left", padding: "3%", color:"black"}}>Floor Tickets</p>
-			            <p className="event-location" style = {{fontWeight: "bold", fontSize: "1vw", textAlign: "left", padding: "3%", color:"black"}}>50/50 available</p>
+			            <p className="event-header" style = {{fontWeight: "bold", fontSize: "1.5vw", textAlign: "left", padding: "3%", color:"black"}}>General Admission</p>
+			            <p className="event-price" style = {{fontWeight: "bold", fontSize: "1vw", textAlign: "left", padding: "3%", color:"black"}}>Price: ${genadTicketPrice}</p>
+			            <p className="event-location" style = {{fontWeight: "bold", fontSize: "1vw", textAlign: "left", padding: "3%", color:"black"}}>{genadTicketsLeft} available</p>
 			            <div className="box">
 			                <button className="reserve-button">Reserve</button>
 			            </div>
 			        </div>
 			        <div className="ticket">
 			            <p className="event-header" style = {{fontWeight: "bold", fontSize: "1.5vw", textAlign: "left", padding: "3%", color:"black"}}>VIP Admission</p>
-			            <p className="event-location" style = {{fontWeight: "bold", fontSize: "1vw", textAlign: "left", padding: "3%", color:"black"}}>50/50 available</p>
+			            <p className="event-price" style = {{fontWeight: "bold", fontSize: "1vw", textAlign: "left", padding: "3%", color:"black"}}>Price: ${vipTicketPrice}</p>
+			            <p className="event-location" style = {{fontWeight: "bold", fontSize: "1vw", textAlign: "left", padding: "3%", color:"black"}}>{vipTicketsLeft} available</p>
 			            <div className="box">
 			                <button className="reserve-button">Reserve</button>
 			            </div>
